291386749b63142517b1adca970242e2ead0fcdf
Ossi Tiltti <ossi.tiltti@gmail.com>
2017-12-16 09:20:27 +0200

    fix output

--- rpi_cpu_temp.sh
+++ rpi_cpu_temp.sh
@@ -5,6 +5,6 @@ cpuTemp2=$(($cpuTemp0/100))
 cpuTempM=$(($cpuTemp2 % $cpuTemp1))
 CPU=$cpuTemp1"."$cpuTempM
 GPU=$(/opt/vc/bin/vcgencmd measure_temp | tr -cd '0-9.')
- 
+echo "CPU: " $CPU
+echo "GPU: " $GPU
 curl -i -XPOST 'http://localhost:8086/write?db=rpicputemp' -u admin:KalkkiPetteri1803influx --data-binary 'temperature CPU='$CPU',GPU='$GPU
-
