6b641da185456481897b4b6696f0d76cac257be8
Ossi Tiltti <ossi.tiltti@gmail.com>
2017-12-16 09:06:30 +0200

    Added script to read rpi cpu and gpu temp and push to influxdb

---
+++ rpi_cpu_temp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cpuTemp0=$(cat /sys/class/thermal/thermal_zone0/temp)
+cpuTemp1=$(($cpuTemp0/1000))
+cpuTemp2=$(($cpuTemp0/100))
+cpuTempM=$(($cpuTemp2 % $cpuTemp1))
+CPU=$cpuTemp1"."$cpuTempM
+GPU=$(/opt/vc/bin/vcgencmd measure_temp | tr -cd '0-9.')
+ 
+curl -i -XPOST 'http://localhost:8086/write?db=rpicputemp' -u admin:KalkkiPetteri1803influx --data-binary 'temperature CPU='$CPU',GPU='$GPU
+
